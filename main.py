e60ba4af99ab339ca1c5978c5ba5b2ca86ebfc45
cherpak <evdak.cherpak@proton.me>
2026-03-18 16:23:31 +0300

    init feed_service

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
